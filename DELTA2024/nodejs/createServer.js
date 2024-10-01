f5ac25f2144223a713f6901aa0148bfb0d87cb65
rajeev35 <rajeev2020it049@abesit.edu.in>
2024-10-01 13:51:57 +0530

    Node js Update

--- DELTA2024/nodejs/createServer.js
+++ DELTA2024/nodejs/createServer.js
@@ -2,8 +2,64 @@ const express = require('express')
 const app = express()
 const db = require('./db')
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const Person = require('./models/person');
+const MenuItem = require('./models/MenuItem');
+
+  
+
 app.get('/', function (req, res) {
   res.send('Welcome to My Hotel How can I heelp You? We have List of Menus')
+})
+
+
+app.post('/person', async (req,res)=>{
+    try{
+        const data = req.body
+        const newPerson = new Person(data);
+        const response = await newPerson.save();
+        console.log("Data Saved");
+        res.status(200).json(response)
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: "Internal Error"});
+
+    }
+    // newPerson.name = data.name;
+    // newPerson.age = data.age;
+    // newPerson.gender = data.gender;
+    // newPerson.email = data.email;
+    // newPerson.mobile = data.mobile
+    // newPerson.save((error, savedPerson)=>{
+    //     if(error){
+    //         console.log("Error saving person", error);
+    //         res.status(500).json({error: "Internal server errorr"})
+    //     }else{
+    //         console.log("Data save successfully");
+    //         res.status(200).json(savedPerson);
+    //     }
+    // })
+
+
+})
+
+
+app.get('/person', async (req, res) =>{
+    try{
+
+        const data = await Person.find();
+        console.log('Data find');
+        res.status(200).json(data);
+
+    }catch(err){
+
+        console.log(err);
+        res.status(500).json({error: "Internal Error"});
+
+    }
 })
 
 
@@ -34,7 +90,7 @@ app.listen(3000, ()=>{
 
 // app.get('/', function(rreq, res){
 //     request.send('Hello World')
-// })
+// }) 
 
 // app.listen(3000,() =>{
 //     console.log("Server is Running at port Number 3000");
---
+++ DELTA2024/nodejs/models/Person.js
@@ -0,0 +1,45 @@
+const { string } = require('i/lib/util');
+const mongoose = require('mongoose');
+//ddefine the person schema 
+
+const personSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+
+    }, 
+    age:{
+        type: Number,
+        require: true
+    },
+    work: {
+        type: String,
+        enum: ['chef', 'waiter', 'manager'],
+        require: true
+
+    },
+    mobile: {
+        type: String,
+        require: true
+    },
+
+    email: {
+       type: String,
+       require: true,
+       unique: true
+    },
+
+    address:{
+        type: String,
+        require: true
+    },
+
+    salary: {
+        type: Number,
+        require: true
+    }
+
+})
+
+const Person = mongoose.model('person', personSchema);
+module.exports = Person;
